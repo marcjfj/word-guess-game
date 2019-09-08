505438d2c9c3771de7b3e56fb30878660d58a20c
Marc <marcjfj@gmail.com>
2019-09-08 14:01:22 -0500

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bob's Bad Day
+## A love story
---
+++ script.js
@@ -0,0 +1,227 @@
+var introHead = document.querySelector(".headline");
+var introText = document.querySelector(".intro-text");
+var introImage = document.querySelector(".intro-image");
+var phraseBox = document.querySelector(".phrase");
+var alphabetBox = document.querySelector(".letters");
+var sceneText = document.querySelector(".scene-text");
+var sceneImage = document.querySelector(".scene-image");
+var startButton = document.querySelector(".start");
+var intro = document.querySelector(".intro");
+var roundCount = document.querySelector(".round");
+var guessCount = document.querySelector(".guesses");
+var round = 0;
+var phrase, phraseArray, guessed, lives;
+var loopTrack = new Audio('loop.wav');
+var failTrack = new Audio('fail.wav');
+var winTrack = new Audio('win.wav');
+var correctTrack = new Audio('correct.wav');
+var wrongTrack = new Audio('wrong.wav');
+loopTrack.loop = true;
+var matches = [];
+var badGuesses = [];
+
+
+
+var game = {
+
+    alphabet: {
+        string: "abcdefghijklmnopqrstuvwxyz",
+        array: function(){ return this.string.split("")}
+    },
+
+    stages: [
+        {
+            phrase: "malfunction",
+            hint: "Bob gets ready for his morning commute",
+            hintImage: "car.svg",
+            winText: "Bob decides walk to work today",
+            winImage: "walk.svg",
+            loseText: "Bob's brakes malfunction and he rear-ends a lawyer.",
+            loseImage: "fender.svg"
+        },
+        {
+            phrase: "terminated",
+            hint: "Bob arrives to work.",
+            hintImage: "work.svg",
+            winText: "Bob decides he doesn't really like working, instead he goes fishing",
+            winImage: "fishing.svg",
+            loseText: "Bob finds out that his job has been made redundant by machines. He is terminated",
+            loseImage: "fired.svg"
+        },
+        {
+            phrase: "infidelity",
+            hint: "Bob decides to surprise his girlfriend with flowers",
+            hintImage: "gf.svg",
+            winText: "Bob decides he doesn't really like his girlfriend, instead goes to a strip club",
+            winImage: "club.svg",
+            loseText: "Bob finds his girlfriend in bed with his boss",
+            loseImage: "bed.svg"
+        },
+        {
+            phrase: "bereavement",
+            hint: "Bob heads home to spend time with his beloved cat",
+            hintImage: "home.svg",
+            winText: "Bob decides he doesn't really like his cat, instead goes to the strip club",
+            winImage: "club.svg",
+            loseText: "Bob realizes that he hasn't fed his cat in weeks",
+            loseImage: "cat.svg"
+        },
+        {
+            phrase: "diagnosed",
+            hint: "Bob gets a phone call",
+            hintImage: "phone.svg",
+            winText: "Bob doesn't answer the phone.",
+            winImage: "club.svg",
+            loseText: "It's his doctor. It turns out that the lump that he found isn't benign after all.",
+            loseImage: "doctor.svg"
+        },
+    ],
+    clear: function(){
+        phraseBox.innerHTML = "";
+        alphabetBox.innerHTML = "";
+        matches = [];
+        badGuesses = [];
+        
+    },
+    initialize: function(round){
+        game.clear();
+        lives = 5;
+        roundCount.textContent = round + 1;
+        guessCount.textContent = lives;
+        sceneText.textContent = game.stages[round].hint;
+        sceneImage.src = game.stages[round].hintImage;
+        phrase = game.stages[round].phrase;
+        phraseArray = phrase.split("");
+        loopTrack.play();
+        
+        phraseArray.forEach(function(letter){
+            var letterBox = document.createElement("div");
+            letterBox.classList.add("letter-box");
+            // letterBox.textContent = letter;
+            phraseBox.appendChild(letterBox);
+        });
+        
+        game.alphabet.array().forEach(function(letter){
+            var letterButton = document.createElement("button");
+            letterButton.classList.add("letter-button");
+            letterButton.value = letter;
+            letterButton.textContent = letter;
+            alphabetBox.appendChild(letterButton);
+        });
+        var letterButtons = document.querySelectorAll(".letter-button");
+        letterButtons.forEach(function(button){
+            button.addEventListener("click", game.guess.bind(null, button.value), false);
+    
+        });
+        document.addEventListener("keydown", game.guess);
+
+    },
+    wrong: function(letter){
+        wrongTrack.currentTime = 0;
+        wrongTrack.play();
+        var alphabetNodes = document.querySelectorAll(".letter-button");
+        alphabetNodes.forEach(function(button){
+            if (button.value === letter){
+                button.classList.add("wrong");
+            }
+        })
+        lives--;
+        guessCount.textContent = lives;
+        if (lives === 0){
+            game.endScene(false);
+            
+        }
+    },
+    match: function(letter){
+        correctTrack.currentTime = 0;
+        correctTrack.play();
+        var phraseNodes = document.querySelectorAll(".letter-box");
+        phraseArray.forEach(function(unit, i){
+            if (unit === letter){
+                
+                phraseNodes[i].textContent = letter;
+            }
+        });
+        var alphabetNodes = document.querySelectorAll(".letter-button");
+        alphabetNodes.forEach(function(button){
+            if (button.value === letter){
+                button.classList.add("matched");
+            }
+        })
+    },
+    guess: function(letter){
+        // console.log(letter);
+        if (letter.key){
+            letter = letter.key.toLowerCase();
+        }
+        if (game.alphabet.array().indexOf(letter) === -1){
+            return;
+        }
+        if (matches.indexOf(letter) < 0 && badGuesses.indexOf(letter) < 0){
+            
+            var match = phraseArray.indexOf(letter);
+            if (match !== -1){
+                while (match !== -1){
+                    game.match(letter);
+                    matches.push(letter);
+                    match = phraseArray.indexOf(letter, match + 1);
+                }
+                if (matches.length === phraseArray.length){
+                    
+                    game.endScene(true);
+                }
+            }else{
+                badGuesses.push(letter);
+                console.log(badGuesses);
+                game.wrong(letter);
+            }
+            console.log(matches);
+        }
+    },
+
+    endScene: function(win){
+        document.removeEventListener("keydown", game.guess);
+        if (win){
+            introHead.textContent = "You Did it!"
+            introText.textContent = game.stages[round].winText;
+            introImage.src = game.stages[round].winImage;
+            loopTrack.pause();
+            loopTrack.currentTime = 0;
+            winTrack.currentTime = 0;
+            winTrack.play();
+            
+
+        }else{
+            introHead.textContent = "You Failed..."
+            introText.textContent = game.stages[round].loseText;
+            introImage.src = game.stages[round].loseImage;
+            loopTrack.pause();
+            loopTrack.currentTime = 0;
+            failTrack.currentTime = 0;
+            failTrack.play();
+            
+        }
+        if ((game.stages.length - 1) === round){
+            startButton.textContent = "Start Over";     
+            round = -1;            
+        }else{
+            startButton.textContent = "Next Round";
+        }    
+        intro.classList.remove("move-away");
+        startButton.disabled = false;
+        startButton.focus();
+        round++;   
+    }
+    
+    
+}
+
+
+startButton.addEventListener("click", function(){
+    intro.classList.add("move-away");
+    startButton.disabled = true;
+    game.initialize(round);
+})
+
+
+
